584e361fe7bae1df0a9825b74fd02d47fbc487be
Glory <gloryope247@gmail.com>
2025-09-18 12:46:49 +0100

    hero session

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='w-11/12 max-w-3xl text-center m-auto h-screen flex flex-col items-center justify-center gap-4'>
       <div>
         <Image src={assets.profile_img} alt=''
         className='rounded-full w-32' />
@@ -18,6 +18,16 @@ const Header = () => {
         <p className='max-w-2xl mx-auto font-ovo'>
           Transforming Ideas and Concepts into real projects that Deliver Impactful Results.
         </p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+          <a href='#contact'
+          className='x-10 py-3 border rounded-full bg-black text-white flex items-center gap-2'>
+            Contact Me <Image src={assets.right_arrow_white} alt=''
+        className='w-4' /></a>
+
+        <a href='#contact' 
+        download className='px-10 py-3 border rounded-full border-gray-50 flex items-center gap-2'>My resume <Image src={assets.download_icon} alt=''
+        className='w-4' /></a>
+        </div>
     </div>
   )
 }
